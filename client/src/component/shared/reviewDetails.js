be7b9fe4c2f0bf0546b7d08e4fbda7d91de3fc0c
junyang <yang.jun3@husky.neu.edu>
2020-07-28 17:44:34 -0400

    update review star

--- client/src/component/shared/reviewDetails.js
+++ client/src/component/shared/reviewDetails.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import "./shared.css";
 import axios from "axios";
 import moment from "moment";
+import ReviewStar from "./reviewStar";
 
 export default class ReviewDetails extends Component {
   state = {
     review: {},
     author: {},
+    ratio: 0,
   };
   componentDidMount() {
     axios
       .get(`/explore/review/details/${this.props.review._id}`)
       .then((res) => {
-        this.setState({ review: res.data.review, author: res.data.author });
+        this.setState({
+          review: res.data.review,
+          author: res.data.author,
+          ratio: (res.data.review.star / 5).toFixed(2) * 100,
+        });
       });
   }
   render() {
@@ -25,7 +31,10 @@ export default class ReviewDetails extends Component {
             src={this.state.author.pic}
           ></img>
           <div className="author-detail">
-            <div>{this.state.author.username}</div>
+            <div className="review-star-block">
+              <span className="author-name">{this.state.author.username}</span>
+              <ReviewStar percentage={this.state.ratio} />
+            </div>
             <div>{moment(this.state.review.date).format("YYYY-MM-DD")}</div>
           </div>
         </div>
