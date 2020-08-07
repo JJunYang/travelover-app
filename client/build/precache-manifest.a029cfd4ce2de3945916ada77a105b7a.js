5469e86dd5e813d8ea49f9bbe25dc2de80af33d4
junyang <yang.jun3@husky.neu.edu>
2020-08-07 17:36:20 -0400

    add build

---
+++ client/build/precache-manifest.a029cfd4ce2de3945916ada77a105b7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d92141797c729e7c85c564a475c015c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7455b539be42f0caf0",
+    "url": "/static/css/2.d2893f41.chunk.css"
+  },
+  {
+    "revision": "6c5e2547cf063f368a4c",
+    "url": "/static/css/main.95ab80e1.chunk.css"
+  },
+  {
+    "revision": "6d7455b539be42f0caf0",
+    "url": "/static/js/2.28521722.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/static/js/2.28521722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5e2547cf063f368a4c",
+    "url": "/static/js/main.88fffe0f.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
